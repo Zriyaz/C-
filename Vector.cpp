6a45f9fddd83ae2f98b9657c920eb0b52ad75d40
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-13 02:38:19 +0000

    /* Reverse the vector*/

--- Vector.cpp
+++ Vector.cpp
@@ -12,9 +12,10 @@ int main()
       }
       for(auto i=v1.begin();i!=v1.end();i++)
       cout<< *i<<endl;
-      
+      /* Reverse the vector*/
+
       for(auto i=v1.rbegin();i!=v1.rend();i++)
       cout<<*i<<" ";
-
       cout<<v1.size();
+
 }
