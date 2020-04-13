c884e72e8d3e264e12bb42f40467e38372f53e79
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-13 02:30:25 +0000

    Initial commit

---
+++ Vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+      int n;
+      vector<int> v1;
+      for(int i=0;i<5;i++){
+        cin>>n;
+        v1.push_back(n);
+      }
+      for(auto i=v1.begin();i!=v1.end();i++)
+      cout<< *i<<endl;
+
+      cout<<v1.size();
+}
